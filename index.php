99928ca3eb015df9f196f1e91176b99eeb5161d4
root <root@jons-linode>
2025-02-07 15:13:58 +0000

    remove unused code

--- index.php
+++ index.php
@@ -171,12 +171,6 @@ $(function(){
       return arr.join("\n");
     }
 
-    function isNumeric(str) {
-      if (typeof str != "string") return false // we only process strings!
-      return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-             !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
-    }
-
     function output(msg){
         $("#output").empty();
         $("#output").append(msg);
